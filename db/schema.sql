d7ab975aa9021bd2d6a1159cf305a7d155f47a8c
Ellyse <85855848+ellysecarter@users.noreply.github.com>
2021-10-30 15:50:08 -0700

    finished models

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,48 @@
--- DROP DATABASE
-DROP DATABASE IF EXISTS ecommerce_db;
 
--- CREATE DATABASE
+DROP DATABASE IF EXISTS ecommerce_db;
 CREATE DATABASE ecommerce_db;
+USE ecommerce_db;
+
+CREATE TABLE category
+(
+    id INTEGER NOT NULL
+    AUTO_INCREMENT,
+    name VARCHAR
+    (30) NOT NULL,
+    PRIMARY KEY
+    (id)
+);
+
+CREATE TABLE product
+(
+    id INTEGER NOT NULL
+        AUTO_INCREMENT,
+        name VARCHAR
+        (30) NOT NULL,
+        PRIMARY KEY
+        (id)
+    );
+
+CREATE TABLE tag
+(
+    id INTEGER NOT NULL
+        AUTO_INCREMENT,
+        name VARCHAR
+        (30) NOT NULL,
+        PRIMARY KEY
+        (id)
+
+    );
+
+CREATE TABLE productTag
+(
+    id INTEGER NOT NULL
+        AUTO_INCREMENT,
+        name VARCHAR
+        (30) NOT NULL,
+        PRIMARY KEY
+        (id)
+
+    );
+
 
